c78d447f7cb9b76b71ea6fc4b7629d2cf4777bcd
Max <maxim21214@gmail.com>
2016-11-21 14:57:52 +0700

    Added compatibility with ember-cli-fastboot
    Fix error "JQuery is not defined" when start ember-fastboot server

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ module.exports = {
   name: 'ember-cli-range-input',
 
   included: function(app) {
-    app.import('vendor/rangeinput.js');
-    app.import('vendor/rangeinput.css');
+    if (!process.env.EMBER_CLI_FASTBOOT) {
+      app.import('vendor/rangeinput.js');
+      app.import('vendor/rangeinput.css');
+    }
   }
 };
